f6480f066b210e4f2a27a7adc589fd3bae103f3b
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-21 20:19:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 require_relative './course.rb'
 
 class Scraper
-  attr_reader :page
+  attr_accessor :page
   def get_page 
     @page = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
     @page = Nokogiri::HTML(page)
