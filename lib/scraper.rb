65be9902202604112b93e804f07c9a9e91318f8b
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-21 20:44:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,10 +17,10 @@ class Scraper
   
   def make_courses
     course_info = self.get_courses
-    p course_info[0].text
     course_titles = course_info.css("h2")
     course_schedules = course_info.css(".date")
     course_descriptions = course_info.css("p")
+    p course_descriptions[0].text
     course_info.each_with_index do |course, index|
       course = Course.new()
       course.description = course_descriptions[index].text
